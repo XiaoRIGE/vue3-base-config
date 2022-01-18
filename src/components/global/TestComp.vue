e3a00db60c63a3527afc9ff78ceea77a995591e5
song rongjie <songrongjie@ledreamer.com>
2022-01-18 15:47:54 +0800

    feat(components): 新增全局组件批量注册

---
+++ src/components/global/TestComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="name">
+    <h1>test component</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "name",
+  setup() {
+    return {};
+  },
+});
+</script>
