b5c3e3cacdc1530f256a18b69e5b33bc57289205
song rongjie <songrongjie@ledreamer.com>
2022-04-26 15:20:43 +0800

    build(components): 引入iconfont

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
+import { StaticConfig } from "@/config/app";
+import { createFromIconfontCN } from "@ant-design/icons-vue";
+// iconfont的使用姿势: 2x.antdv.com/components/icon-cn/#components-icon-demo-use-iconfont.cn
+const IconFont: any = createFromIconfontCN({
+  scriptUrl: StaticConfig.IconfontURL,
+});
 
 /**
  * @description 自动将 ./src/components/global 下的组件注册成为全局组件
@@ -14,4 +20,7 @@ export const registerGlobalComponent = function (
     const name = key.replace(/^\.\//, "").replace(/\.\w+$/, "");
     app.component(name, config.default || config);
   });
+
+  // 全局注册 iconfont
+  app.component("IconFont", IconFont);
 };
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,11 +1,12 @@
 /**  跟应用全局相关的静态配置放在这里  */
-import { message } from "ant-design-vue";
+// import { message } from "ant-design-vue";
 
-const AppConfig = {
-  $message: message,
-};
+// const AppConfig = {
+//   $message: message,
+// };
+
 const StaticConfig = {
   MaxPageSize: 1000,
   IconfontURL: "//at.alicdn.com/t/font_2529778_on5r7wtieq9.js",
 };
-export { AppConfig, StaticConfig };
+export { StaticConfig };
